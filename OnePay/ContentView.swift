fd95a98c9b7e58c056805898dae4b21b85a6cd01
Evhen Gruzinov <evhen@gruzinov.com>
2023-07-11 12:01:04 +0200

    Initial Commit

---
+++ OnePay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OnePay
+//
+//  Created by Evhen Gruzinov on 11.07.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OnePay/OnePayApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnePayApp.swift
+//  OnePay
+//
+//  Created by Evhen Gruzinov on 11.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct OnePayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
